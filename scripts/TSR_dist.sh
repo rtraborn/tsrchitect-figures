37e5ebd0c84f9ec623a2f76a1949df312c7fb1eb
rtraborn <rtraborn@indiana.edu>
2017-10-04 17:49:52 -0400

    Added AtEST data and new script.

--- scripts/TSR_dist.sh
+++ scripts/TSR_dist.sh
@@ -12,18 +12,31 @@ TokCAGE_m=/scratch/rtraborn/TSRchitect_plant_results/Arabidopsis/Tokizawa_CAGE/T
 TokVec_dir=/scratch/rtraborn/TSRchitect_plant_results/Arabidopsis/Tokizawa_Vec_capping
 TokVec_m=/scratch/rtraborn/TSRchitect_plant_results/Arabidopsis/Tokizawa_Vec_capping/TSSset-1_sum.bed
 
+GenbankEST=/projects/TSRplants/ESTcDNA/A_thaliana/tsrOut/AtEST_genbank_out/TSRset-1.bed
+
 At_GROseq=/scratch/rtraborn/TSRchitect_plant_results/plant_genomic_data/A_thaliana/GROseq/At_GROseq_out.bed
 At_TAIR_gff=/projects/rtraborn/plant_promoters/A_thaliana/genome/TAIR10_GFF3_genes.gff
-At_TAIR_gene=/projects/rtraborn/plant_promoters/A_thaliana/genome/TAIR10_GFF3_genes.bed
+At_TAIR_genes=/projects/rtraborn/plant_promoters/A_thaliana/genome/TAIR10_genes.bed
+At_TAIR_genes_nuc=TAIR10_genes_nuc.bed
 
-#gff2bed < $At_TAIR_Gff > $At_TAIR_gene
+#gff2bed < $At_TAIR_gff > $At_TAIR_genes
 
-bedtools closest -s -D a  -a $PEAT_m -b $At_GROseq > PEAT_closest_GRO.txt
-bedtools closest -s -D a  -a $TokCAGE_m -b $At_GROseq > TokCAGE_closest_GRO.txt
-bedtools closest -s -D a  -a $TokVec_m -b $At_GROseq > TokVec_closest_GRO.txt
+#head $At_TAIR_genes
 
-bedtools closest -s -D a  -a $PEAT_m -b $At_TAIR_gene > PEAT_closest_gene.txt
-bedtools closest -s -D a  -a $TokCAGE_m -b $At_TAIR_gene > TokCAGE_closest_gene.txt
-bedtools closest -s -D a  -a $TokVec_m -b $At_TAIR_gene > TokVec_closest_gene.txt
+cat $At_TAIR_genes | awk 'BEGIN{OFS="\t";} $1!="choloroplast" {print }' > $At_TAIR_genes_nuc
+
+head $At_TAIR_genes_nuc
+
+#bedtools closest -s -Da  -a $PEAT_m -b $At_GROseq > PEAT_closest_GRO.txt
+#bedtools closest -s -Da  -a $TokCAGE_m -b $At_GROseq > TokCAGE_closest_GRO.txt
+#bedtools closest -s -Da  -a $TokVec_m -b $At_GROseq > TokVec_closest_GRO.txt
+
+#bedtools closest -s -Da  -a $PEAT_m -b $At_TAIR_gene > PEAT_closest_gene.txt
+#bedtools closest -s -Da  -a $TokCAGE_m -b $At_TAIR_gene > TokCAGE_closest_gene.txt
+#bedtools closest -s -Da  -a $TokVec_m -b $At_TAIR_gene > TokVec_closest_gene.txt
+
+bedtools closest -s -iu -D a -a $GenbankEST -b $At_TAIR_genes_nuc > AtEST_closest_gene.txt
+cat AtEST_closest_gene.txt | awk 'BEGIN{OFS="\t";} $17!="-1" {print }' > AtEST_closest_gene_i.txt
+mv AtEST_closest_gene_i.txt /scratch/rtraborn/tsrchitect-figures/output_files
 
 echo "Job Complete!"
--- scripts/genes_per_dataset.R
+++ scripts/genes_per_dataset.R
@@ -1,3 +1,20 @@
 ## Measuring the number of promoters identified using TSRchtect, and the total gene coverage across the 4 plant experiments (2 species)
 
+library(ggplot2)
+library(GenomicRanges)
+
+AtEST.file <- "/scratch/rtraborn/tsrchitect-figures/output_files/AtEST_closest_gene_i.txt"
+AtESTdist <- read.table(file=AtEST.file, header=FALSE)
+
+head(AtESTdist)
+
+colnames(AtESTdist) <- c("chr","start1","end1", "ID1", "score", "strand1", "chr2", "start2", "end2", "ID2", "score2", "strand2", "assemblyID", "type", "score3", "ID_string", "distance")
+
+head(AtESTdist)
+
+a <- ggplot(AtESTdist, aes(distance))
+a + geom_histogram(binwidth=10)
+
+ggsave("AtEST_closest.png")
+
 
