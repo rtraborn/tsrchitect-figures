ae38206f832b78bebe6bf06d4902c7d6a9950e2f
rtraborn <rtraborn@indiana.edu>
2017-10-09 12:27:04 -0400

    Modified distance_classes figure take 2.

--- scripts/genes_per_dataset.R
+++ scripts/genes_per_dataset.R
@@ -19,19 +19,18 @@ a + geom_histogram()
 
 ggsave("AtEST_closest.png")
 
-n.prom <- matrix(NA, nrow=4, ncol=3)
+n.prom <- matrix(NA, nrow=4, ncol=2)
 
 under100 <- length(which(AtESTdist[,17]<100))
 under500 <- length(which(AtESTdist[,17]<500))
 under1000 <- length(which(AtESTdist[,17]<1000))
 above1k  <- length(which(AtESTdist[,17]>1000))
 
-n.prom[,1] <- c("AtEST")                   
+n.prom[,1] <- c("under100", "under500", "under1000", "above1k")
 n.prom[,2] <- c(under100, under500, under1000, above1k)
-n.prom[,3] <- c("under100", "under500", "under1000", "above1k")
 
 n.prom <- as.data.frame(n.prom)
-colnames(n.prom) <- c("dataset", "class", "distance")
+colnames(n.prom) <- c("class", "distance")
                    
 head(n.prom)
 
